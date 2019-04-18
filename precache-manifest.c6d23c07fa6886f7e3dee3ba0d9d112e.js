9574bc7699d659f6bad160aef8c6b2ee3c54ca29
Mia Xia <mxia@apple.com>
2019-04-18 15:08:52 -0700

    Updates

---
+++ precache-manifest.c6d23c07fa6886f7e3dee3ba0d9d112e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "60d6be270239af0786f6",
+    "url": "/static/js/main.9a4cf7aa.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "60d6be270239af0786f6",
+    "url": "/static/css/main.19153a92.chunk.css"
+  },
+  {
+    "revision": "ba3750555e8bb735ad0bb953007ff521",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6b6e9c9907e33e8e27ac52955b38cd6f.js"
+  "/precache-manifest.c6d23c07fa6886f7e3dee3ba0d9d112e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
