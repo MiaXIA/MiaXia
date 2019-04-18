fc02f3066123cb17733f7ce762d576f70afb567f
Mia Xia <mxia@apple.com>
2019-04-18 14:54:56 -0700

    try deploy

---
+++ build/precache-manifest.6b6e9c9907e33e8e27ac52955b38cd6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "69dc666f8b9df3c9b85f",
+    "url": "./static/js/main.ac21d89b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "./static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "69dc666f8b9df3c9b85f",
+    "url": "./static/css/main.19153a92.chunk.css"
+  },
+  {
+    "revision": "91456d151c244057e3627c33995459dd",
+    "url": "./index.html"
+  }
+];
