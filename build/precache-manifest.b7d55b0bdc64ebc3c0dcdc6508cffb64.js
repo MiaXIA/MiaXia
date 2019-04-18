877c177cc8cdc54a18a97d487719796b51780469
Mia Xia <mxia@apple.com>
2019-04-18 15:16:30 -0700

    Try deploy one more time

---
+++ build/precache-manifest.b7d55b0bdc64ebc3c0dcdc6508cffb64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MiaXia/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "099442cdf006ba5de389",
+    "url": "/MiaXia/static/js/runtime~main.95f5b23c.js"
+  },
+  {
+    "revision": "f6f721ac3693b257b911",
+    "url": "/MiaXia/static/js/main.cb2ee229.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/MiaXia/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f6f721ac3693b257b911",
+    "url": "/MiaXia/static/css/main.19153a92.chunk.css"
+  },
+  {
+    "revision": "e0d9f4731e963c2d069ef517d0b9c574",
+    "url": "/MiaXia/index.html"
+  }
+];
